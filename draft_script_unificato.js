e441ed1660a79dd6084ed2ad46199193764db770
Ccezzionale <tringali0511@gmail.com>
2025-07-29 18:02:07 +0200

    Update draft_script_unificato.js

--- draft_script_unificato.js
+++ draft_script_unificato.js
@@ -187,12 +187,9 @@ if (celle.length >= 3 && !celle[2].textContent.trim()) {
   inviaPickAlFoglio(pick, fantaTeam, nome, ruolo, squadra, quotazione);
   break;
 }
-        tr.remove();
-      }
-    });
-    listaGiocatori.appendChild(tr);
-  });
-}
+  tr.remove();
+  listaGiocatori.appendChild(tr);
+});
 
   Array.from(ruoli).forEach(r => {
     const opt = document.createElement("option");
