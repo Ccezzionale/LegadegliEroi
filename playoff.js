fdd2ef1f61639a5de44c451eaf2f8511c9f1f43d
Ccezzionale <tringali0511@gmail.com>
2025-08-20 14:52:28 +0200

    Update playoff.js

--- playoff.js
+++ playoff.js
@@ -73,20 +73,20 @@ function creaHTMLSquadra(nome, posizione = "", punteggio = "", isVincente = fals
 /* 1) EDITA QUI per indicare i vincitori */
 const PICKS = {
   // Wildcard
-  WC1: { home: '', away: '' },
-  WC2: { home: '', away: '' },
-  WC3: { home: '', away: '' },
-  WC4: { home: '', away: '' },
+  WC1: { home: 'W', away: '' },
+  WC2: { home: '', away: 'W' },
+  WC3: { home: '', away: 'W' },
+  WC4: { home: 'W', away: '' },
   // Quarti
-  Q1:  { home: '', away: '' },
-  Q2:  { home: '', away: '' },
-  Q3:  { home: '', away: '' },
-  Q4:  { home: '', away: '' },
+  Q1:  { home: '', away: 'W' },
+  Q2:  { home: 'W', away: '' },
+  Q3:  { home: '', away: 'W' },
+  Q4:  { home: 'W', away: '' },
   // Semifinali
-  S1:  { home: '', away: '' },
-  S2:  { home: '', away: '' },
+  S1:  { home: 'W', away: '' },
+  S2:  { home: '', away: 'W' },
   // Finale
-  F:   { home: '', away: '' },
+  F:   { home: 'W', away: '' },
 };
 
 /* 2) Abbinamenti Wildcard (indici 0-based nella classifica ordinata) */
