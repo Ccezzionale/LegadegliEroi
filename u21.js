49b1402528123271af4bb014ab4840120e15b19b
Ccezzionale <tringali0511@gmail.com>
2026-02-02 15:32:18 +0100

    Create u21.js

---
+++ u21.js
@@ -0,0 +1,20 @@
+// SOLO DATI U21 (nessun addEventListener, nessuna logica pagina rose)
+
+const giocatoriU21PerSquadra = {
+  "Team Bartowski": ["denoon", "gineitis", "mateus lusuardi"],
+  "Desperados": ["ordonez c.", "lipani", "ramon"],
+  "Riverfilo": ["paz n.", "castro s.", "akinsanmiro"],
+  "Golden Knights": ["palestra", "camarda", "esposito f.p."],
+  "Fantaugusta": ["comuzzo", "pisilli", "ekhator"],
+  "Fc Disoneste": ["gineitis", "norton-cuffy", "ziolkowski"],
+  "Rubinkebab": ["diao", "pisilli", "ahanor"],
+  "Eintracht Franco 126": ["marianucci", "valle", "ramon"],
+  "PokerMantra": ["ziolkowski", "goglichidze", "belahyane"],
+  "wildboys78": ["rodriguez je.", "tiago gabriel", "ferguson e."],
+  "Bayern Christiansen": ["paz n.", "palestra", "rodriguez je."],
+  "Minnesode Timberland": ["diao", "ferguson e.", "britschgi"],
+  "Athletic Pongao": ["valle", "tiago gabriel", "norton-cuffy"],
+  "MinneSota Snakes": ["esposito f.p.", "comuzzo", "ndour"],
+  "Ibla": ["camarda", "belahyane", "bartesaghi"],
+  "Pandinicoccolosini": ["cham", "ahanor", "adzic"]
+};
